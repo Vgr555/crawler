c8cc0fdd5a3f0eeeb74cf3cded4e2939d0bd15e6
Vgr555 <myoneplus003@gmail.com>
2024-02-25 10:26:35 +0530

    modified the code to crawl to given certain depth

--- src/main/java/org/crawler/Crawler.java
+++ src/main/java/org/crawler/Crawler.java
@@ -11,7 +11,9 @@ import java.net.URISyntaxException;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Hello world!
@@ -19,25 +21,27 @@ import java.util.List;
  */
 public class Crawler
 {
+    private static final int DEPTH = 2;
+    private static int count = 0;
     public static void main( String[] args )
     {
-        String url = "https://itlumee.in/";
+
+        String url = "https://www.cricbuzz.com/";
         // Connect to the URL and get the HTML document
         LocalDateTime start = LocalDateTime.now();
-        List<String> links = getLinks(url);
-        System.out.println(links);
-        if (links.isEmpty()) return;
-        for (int i = 0; i < links.size(); i++) {
-            String s = links.get(i);
-            System.out.println((i + 1) + " " + s);
-            getLinks(s);
-        }
+        Set<String> visitedLinks = new HashSet<>();
+        getLinks(url, 0, visitedLinks);
         LocalDateTime end = LocalDateTime.now();
         Duration timeTaken = Duration.between(start, end);
         System.out.println("time taken to crawl: " + timeTaken.getSeconds());
     }
 
-    public static List<String> getLinks(String url) {
+    public static void getLinks(String url, int depth, Set<String> visitedLinks) {
+        if(depth > DEPTH || visitedLinks.contains(url)){
+            return;
+        }
+        System.out.println(++count + " " + url);
+        visitedLinks.add(url);
         List<String> resLinks = new ArrayList<>();
         try {
             Document document = Jsoup.connect(url).get();
@@ -47,6 +51,9 @@ public class Crawler
                 try {
                     URI uri = new URI(href);
                     if (uri.getScheme() != null && uri.getHost() != null) {
+                        if(url.equals(href+"/")){
+                            continue;
+                        }
                         resLinks.add(href);
                         System.out.println("    " + href);
                     }
@@ -58,6 +65,8 @@ public class Crawler
             System.out.println("Error in: " + url + ": cause: " + ex.getCause());
         }
 
-        return resLinks;
+        for(String link : resLinks){
+            getLinks(link, depth+1, visitedLinks);
+        }
     }
 }
